1b5d140b133978f149e42b6b539766488f0489a2
Hasib <hasib.jvai@gmail.com>
2025-08-04 06:01:48 +0600

    first commit

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ErrorPage() {
+  return (
+    <div className='text-center'>Page Not Found</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Layout/Root.jsx';
+import ErrorPage from './Pages/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path:"/",
+        element:<App />,
+      },
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
